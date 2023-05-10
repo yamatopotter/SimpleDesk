2d0a3c1734516256a1c96e889a3210ce7463e787
Matheus Barreto <matheuspbarreto@hotmail.com.br>
2023-05-10 10:41:32 -0300

    add: Backend initial files

---
+++ backend/src/main/java/simpledesk/app/SimpleDeskApplication.java
@@ -0,0 +1,13 @@
+package simpledesk.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleDeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleDeskApplication.class, args);
+	}
+
+}
