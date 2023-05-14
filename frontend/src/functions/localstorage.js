64bfa88ed513bee0a083296c4d71a5f6acd867c1
Matheus Barreto <matheuspbarreto@hotmail.com.br>
2023-05-14 18:14:06 -0300

    add: criação do provider

--- frontend/src/functions/localstorage.js
+++ frontend/src/functions/localstorage.js
@@ -1,3 +1,7 @@
-const localStorageData = {
-  token: "token",
+export const setLocalstorageData = (data, value) => {
+  localStorage.setItem(data, value);
+};
+
+export const getLocalstorageData = (data) => {
+  return localStorage.getItem(data);
 };
