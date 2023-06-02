17cd59bfd8b1eadbc78ac8d1847f95e3e3b10bd1
Douglas Nascimento <douglassnascimet@gmail.com>
2023-06-02 10:31:57 -0300

    add: Adicionando uma componente para criar um usuário administrador por padrão, caso não tenha no banco de dados.

---
+++ backend/src/main/java/simpledesk/app/config/CommandLineAppStartupRunner.java
@@ -0,0 +1,34 @@
+package simpledesk.app.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import simpledesk.app.entity.Role;
+import simpledesk.app.entity.User;
+import simpledesk.app.repository.IUserRepository;
+
+@Component
+public class CommandLineAppStartupRunner implements CommandLineRunner {
+
+    @Autowired
+    IUserRepository userRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Override
+    public void run(String... args) throws Exception {
+        if (userRepository.findByRole(Role.ADMIN).isEmpty()) {
+            var user = User.builder()
+                    .name("Administrador")
+                    .email("administrador@simpledesk.com")
+                    .password(passwordEncoder.encode("admin"))
+                    .phone("(21)91234-5678")
+                    .role(Role.ADMIN)
+                    .build();
+
+            userRepository.save(user);
+        }
+    }
+}
