77d82a47bc6b963cffcc12084baa9b2b7a002639
Douglas Nascimento <douglassnascimet@gmail.com>
2023-09-11 20:33:50 -0300

    refactor: Retirando importação incorreta.

--- backend/src/main/java/simpledesk/app/controller/SectorController.java
+++ backend/src/main/java/simpledesk/app/controller/SectorController.java
@@ -6,7 +6,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
