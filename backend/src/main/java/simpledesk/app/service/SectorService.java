2a19ea2c3c8b4f0872da8b3ce211959ea20190e8
Douglas Nascimento <douglassnascimet@gmail.com>
2023-09-11 20:22:40 -0300

    refactor: Realizando refatoração da entidade Sector.

--- backend/src/main/java/simpledesk/app/service/SectorService.java
+++ backend/src/main/java/simpledesk/app/service/SectorService.java
@@ -2,75 +2,77 @@ package simpledesk.app.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import simpledesk.app.DTO.sector.SectorDTO;
 import simpledesk.app.DTO.sector.SectorDTOMapper;
 import simpledesk.app.entity.Sector;
 import simpledesk.app.repository.ISectorRepository;
+import simpledesk.app.service.exceptions.DataIntegratyViolationException;
+import simpledesk.app.service.exceptions.EmptyAttributeException;
+import simpledesk.app.service.exceptions.ObjectNotFoundException;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Stream;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class SectorService {
-    private final SectorDTOMapper sectorDTOMapper;
-    private final ISectorRepository sectorRepository;
+    private final SectorDTOMapper mapper;
+    private final ISectorRepository repository;
 
+    @Transactional(readOnly = true)
     public List<SectorDTO> findAll() {
-
-        Stream<SectorDTO> sector;
-        sector = sectorRepository.findAll().stream().map(sectorDTOMapper);
-        return sector.toList();
+        return repository.findAll().stream()
+                .map(mapper)
+                .collect(Collectors.toList());
     }
 
-
+    @Transactional(readOnly = true)
     public Optional<SectorDTO> findById(Long id) {
-
-        Optional<SectorDTO> sector;
-        sector = sectorRepository.findById(id).map(sectorDTOMapper);
-        if (sector.isPresent()){
-            return sector;
-        } else {
-            return Optional.of(null);
-        }
+        return Optional.of(repository.findById(id)
+                .map(mapper)
+                .orElseThrow(() -> new ObjectNotFoundException("Setor de ID: " + id + " não encontrado.")));
     }
 
+    @Transactional
     public Optional<SectorDTO> addSector(SectorDTO sector) {
+        emptyAttribute(sector);
+        findByName(sector);
 
-        if (sector == null || sectorRepository.findByName(sector.name()).isPresent()) {
-            return Optional.of(null);
-        } else {
-
-            Sector newSector = sectorRepository.saveAndFlush(
-                    new Sector(
-                            null,
-                            sector.name()
-                    )
-            );
-
-            return Optional.of(sectorDTOMapper.apply(newSector));
-        }
+        Sector newSector = repository.save(new Sector(null, sector.name()));
+        return Optional.of(mapper.apply(newSector));
     }
 
+    @Transactional
+    public Optional<SectorDTO> updateSector(SectorDTO sector) {
+        emptyAttribute(sector);
+        findByName(sector);
+
+        Sector sectorUpdate = new Sector(sector.id(), sector.name());
+        return Optional.of(mapper.apply(repository.save(sectorUpdate)));
+    }
+
+    @Transactional
     public Boolean hardDeleteSector(Long id) {
-        if(sectorRepository.findById(id).isPresent()){
-            sectorRepository.deleteById(id);
+        if (repository.findById(id).isPresent()) {
+            repository.deleteById(id);
             return true;
         }
         return false;
     }
 
-    public Optional<SectorDTO>updateSector(SectorDTO sector){
-        if (sector == null || sectorRepository.findByName(sector.name()).isPresent()) { // Não pode atualizar para um setor já existente
-            return Optional.of(null);
 
-        } else {
-            Sector sectorUpdate = new Sector(
-                    sector.id(),
-                    sector.name()
-            );
-            return Optional.of(sectorDTOMapper.apply(sectorRepository.saveAndFlush(sectorUpdate)));
-        }
+    @Transactional(readOnly = true)
+    public void findByName(SectorDTO sectorDTO) {
+        Optional<Sector> sector = repository.findByName(sectorDTO.name());
+        if (sector.isPresent() && !sector.get().getId().equals(sectorDTO.id()))
+            throw new DataIntegratyViolationException("Setor já cadastrado.");
+    }
+
+
+    public void emptyAttribute(SectorDTO sectorDTO) {
+        if (sectorDTO.name().isEmpty())
+            throw new EmptyAttributeException("Todos os atríbutos são necessários");
     }
 }
