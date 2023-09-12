210183f062175b9c406aa979f4a19fb4e21c553e
Douglas Nascimento <douglassnascimet@gmail.com>
2023-09-11 21:06:58 -0300

    refactor: Refatorando a entidade Equipment.

--- backend/src/main/java/simpledesk/app/SimpleDeskApplication.java
+++ backend/src/main/java/simpledesk/app/SimpleDeskApplication.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.apache.log4j.BasicConfigurator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,6 +13,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @SecurityScheme(name = "Bearer Authentication" , scheme = "bearer", type = SecuritySchemeType.HTTP, bearerFormat = "bearer")
 @OpenAPIDefinition(
+		servers = {
+				@Server(url = "/", description = "Default Server URL")
+		},
 		info = @Info(
 				title = "API REST Simples Desk",
 				version = "1.0.0",
