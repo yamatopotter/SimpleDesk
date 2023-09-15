eab63dc621e326bd0331989ac818f5cafcb40704
Douglas Nascimento <douglassnascimet@gmail.com>
2023-09-15 14:40:16 -0300

    refactor: Melhorando o design do código e implementando validações extras.

--- backend/src/main/java/simpledesk/app/service/TicketHistoryService.java
+++ backend/src/main/java/simpledesk/app/service/TicketHistoryService.java
@@ -5,121 +5,95 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import simpledesk.app.DTO.ticketHistory.TicketHistoryDTO;
 import simpledesk.app.DTO.ticketHistory.TicketHistoryDTOMapper;
-import simpledesk.app.entity.*;
+import simpledesk.app.entity.Status;
+import simpledesk.app.entity.Ticket;
+import simpledesk.app.entity.TicketHistory;
+import simpledesk.app.entity.User;
 import simpledesk.app.repository.IStatusRepository;
 import simpledesk.app.repository.ITicketHistoryRepository;
 import simpledesk.app.repository.ITicketRepository;
 import simpledesk.app.repository.IUserRepository;
+import simpledesk.app.service.exceptions.EmptyAttributeException;
+import simpledesk.app.service.exceptions.ObjectNotFoundException;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
 public class TicketHistoryService {
 
-    private final ITicketHistoryRepository ticketHistoryRepository;
-    private final TicketHistoryDTOMapper ticketHistoryDTOMapper;
+    private final ITicketHistoryRepository repository;
+    private final TicketHistoryDTOMapper mapper;
     private final IUserRepository userRepository;
     private final ITicketRepository ticketRepository;
     private final IStatusRepository statusRepository;
 
-    public List<TicketHistoryDTO> findAll(){
-
-        Stream<TicketHistoryDTO> ticket;
-        ticket = ticketHistoryRepository.findAll().stream().map(ticketHistoryDTOMapper);
-        return ticket.toList();
+    public List<TicketHistoryDTO> findAll() {
+        return repository.findAll().stream().map(mapper).collect(Collectors.toList());
     }
 
     public Optional<TicketHistoryDTO> findById(Long id) {
-
-        Optional<TicketHistoryDTO> ticket;
-        ticket = ticketHistoryRepository.findById(id).map(ticketHistoryDTOMapper);
-        if (ticket.isPresent()){
-            return ticket;
-        } else {
-            return Optional.of(null);
-        }
+        return Optional.of(repository.findById(id)
+                .map(mapper)
+                .orElseThrow(() -> new ObjectNotFoundException("Ticket History de ID: " + id + " não encontrado.")));
     }
 
     public List<TicketHistoryDTO> findByTicket(Ticket ticket) {
-        List<TicketHistoryDTO> ticketHistory;
-        ticketHistory = ticketHistoryRepository.findByTicket(ticket).stream().map(ticketHistoryDTOMapper).collect(Collectors.toList());
-        System.out.println("Ticket History");
-        System.out.println(ticketHistory);
-        return ticketHistory;
+        return repository.findByTicket(ticket).stream().map(mapper).collect(Collectors.toList());
     }
 
     public Optional<TicketHistoryDTO> addTicketHistory(TicketHistoryDTO ticketHistoryDTO) {
-
         Object principal = SecurityContextHolder.getContext().getAuthentication().getName();
-        String idUser = (String) principal;
-        Optional<User> userEntity = Optional.of(userRepository.findByEmail(idUser).get());
+        String user = (String) principal;
+        User userEntity = userRepository.findByEmail(user).orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado."));
 
-        Optional<Ticket> ticketToTicketHistory;
-        Optional<Status> statusToTicketHistory;
+        emptyAttribute(ticketHistoryDTO);
 
-        statusToTicketHistory = statusRepository.findById(ticketHistoryDTO.status().id());
-        ticketToTicketHistory = ticketRepository.findById(ticketHistoryDTO.ticket().id());
+        Ticket ticketToTicketHistory = ticketRepository.findById(ticketHistoryDTO.ticket().id())
+                .orElseThrow(() -> new ObjectNotFoundException("Ticket de ID: " + ticketHistoryDTO.ticket().id() + " não foi encontrado."));
+        Status statusToTicketHistory = statusRepository.findById(ticketHistoryDTO.status().id())
+                .orElseThrow(() -> new ObjectNotFoundException("Status de ID: " + ticketHistoryDTO.status().id() + " não foi encontrado."));
+
+        TicketHistory ticketHistory = repository.saveAndFlush(new TicketHistory(null, userEntity, ticketToTicketHistory,
+                statusToTicketHistory, ticketHistoryDTO.description(), ticketHistoryDTO.urlPhoto(), null));
+        return Optional.of(mapper.apply(ticketHistory));
+
+    }
+
+    public Optional<TicketHistoryDTO> updateTicketHistory(TicketHistoryDTO ticketHistoryDTO) {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getName();
+        String user = (String) principal;
+        User userEntity = userRepository.findByEmail(user).orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado."));
+
+        emptyAttribute(ticketHistoryDTO);
+
+        Ticket ticketToTicketHistory = ticketRepository.findById(ticketHistoryDTO.ticket().id())
+                .orElseThrow(() -> new ObjectNotFoundException("Ticket de ID: " + ticketHistoryDTO.ticket().id() + " não foi encontrado."));
+        Status statusToTicketHistory = statusRepository.findById(ticketHistoryDTO.status().id())
+                .orElseThrow(() -> new ObjectNotFoundException("Status de ID: " + ticketHistoryDTO.status().id() + " não foi encontrado."));
 
 
-        if (ticketHistoryDTO == null) {
-            return Optional.of(null);
-        } else {
-            TicketHistory ticketHistory = ticketHistoryRepository.saveAndFlush(
-                    new TicketHistory(
-                            null,
-                            userEntity.get(),
-                            ticketToTicketHistory.get(),
-                            statusToTicketHistory.get(),
-                            ticketHistoryDTO.description(),
-                            ticketHistoryDTO.urlPhoto(),
-                            null
-                    )
-            );
-            return Optional.of(ticketHistoryDTOMapper.apply(ticketHistory));
-        }
+        TicketHistory ticketHistory = repository.saveAndFlush(new TicketHistory(ticketHistoryDTO.id(), userEntity, ticketToTicketHistory,
+                statusToTicketHistory, ticketHistoryDTO.description(), ticketHistoryDTO.urlPhoto(), null));
+        return Optional.of(mapper.apply(ticketHistory));
     }
 
     public Boolean hardDeleteTicketHistory(Long id) {
-        if(ticketHistoryRepository.findById(id).isPresent()){
-            ticketHistoryRepository.deleteById(id);
+        if (repository.findById(id).isPresent()) {
+            repository.deleteById(id);
             return true;
         }
         return false;
     }
 
-    public Optional<TicketHistoryDTO> updateTicketHistory(TicketHistoryDTO ticket) {
-
-        Object principal = SecurityContextHolder.getContext().getAuthentication().getName();
-        String idUser = (String) principal;
-        Optional<User> userEntity = Optional.of(userRepository.findByEmail(idUser).get());
-
-        Optional<Ticket> ticketToTicketHistory;
-        Optional<Status> statusToTicketHistory;
-
-        statusToTicketHistory = statusRepository.findById(ticket.status().id());
-        ticketToTicketHistory = ticketRepository.findById(ticket.ticket().id());
-
-
-        if (ticket == null) {
-            return Optional.of(null);
-        } else {
-            TicketHistory ticketHistory = ticketHistoryRepository.saveAndFlush(
-                    new TicketHistory(
-                            ticket.id(),
-                            userEntity.get(),
-                            ticketToTicketHistory.get(),
-                            statusToTicketHistory.get(),
-                            ticket.description(),
-                            ticket.urlPhoto(),
-                            ticket.created_at()
-                    )
-            );
-            return Optional.of(ticketHistoryDTOMapper.apply(ticketHistoryRepository.saveAndFlush(ticketHistory)));
-        }
+    public void emptyAttribute(TicketHistoryDTO ticketHistoryDTO) {
+        if (ticketHistoryDTO.description().isEmpty() || ticketHistoryDTO.urlPhoto() == null ||
+                ticketHistoryDTO.ticket() == null || ticketHistoryDTO.ticket().id() == null ||
+                ticketHistoryDTO.status() == null || ticketHistoryDTO.status().id() == null)
+            throw new EmptyAttributeException("Todos os atríbutos são necessários");
     }
+
 
 }
