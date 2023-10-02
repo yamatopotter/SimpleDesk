2e1bd77c4f9838b340ba1cb3437efd4089c1a84b
Douglas Nascimento <douglassnascimet@gmail.com>
2023-10-02 15:51:50 -0300

    refactor: Tratando erro de FK ao excluir equipment vinculado a um ticket.

--- backend/src/main/java/simpledesk/app/repository/ITicketRepository.java
+++ backend/src/main/java/simpledesk/app/repository/ITicketRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+import simpledesk.app.domain.entity.Equipment;
 import simpledesk.app.domain.entity.Status;
 import simpledesk.app.domain.entity.Ticket;
 import simpledesk.app.domain.entity.User;
@@ -26,4 +27,6 @@ public interface ITicketRepository extends JpaRepository<Ticket, Long> {
     List<Ticket> findByUser(User user);
     @Transactional(readOnly = true)
     List<Ticket> findByStatus(Status status);
+    @Transactional(readOnly = true)
+    List<Ticket> findByEquipment(Equipment equipment);
 }
