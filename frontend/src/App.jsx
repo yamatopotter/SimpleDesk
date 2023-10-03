33adc6595c4f0a6fa1ac4a19362304ab6a9a92d3
Matheus Barreto <matheuspbarreto@hotmail.com.br>
2023-10-03 12:56:22 -0300

    wip: Adicionando lazy load nas rotas da aplicação

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,33 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Login } from "./routes/Login";
-import { Home } from "./routes/Home";
-import { Header } from "./components/Header/Header";
-import { About } from "./routes/About";
-import { ListSectors } from "./routes/List/ListSectors";
-import { ListUsers } from "./routes/List/ListUsers";
-import { CreateTicket } from "./routes/Create/CreateTicket";
-import { CreateSector } from "./routes/Create/CreateSector";
-import { CreateStatus } from "./routes/Create/CreateStatus";
-import { CreateEquipment } from "./routes/Create/CreateEquipment";
-import { CreateUser } from "./routes/Create/CreateUser";
-import { DetailTicket } from "./routes/Detail/DetailTicket";
-import { DetailEquipment } from "./routes/Detail/DetailEquipment";
-import { CreateEquipmentType } from "./routes/Create/CreateEquipmentType";
-import { CreateTicketHistory } from "./routes/Create/CreateTicketHistory";
+import { lazyLoad } from "./lazyLoad";
+const Login = lazyLoad("./routes/Login", "Login");
+const Home = lazyLoad("./routes/Home", "Home");
+const Header = lazyLoad("./components/Header/Header", "Header");
+const About = lazyLoad("./routes/About", "About");
+const ListSectors = lazyLoad("./routes/List/ListSectors", "ListSectors");
+const ListUsers = lazyLoad("./routes/List/ListUsers", "ListUsers");
+const ListEquipments = lazyLoad("./routes/List/ListEquipments", "ListEquipments");
+const CreateTicket = lazyLoad("./routes/Create/CreateTicket", "CreateTicket");
+const CreateSector = lazyLoad("./routes/Create/CreateSector", "CreateSector");
+const CreateStatus = lazyLoad("./routes/Create/CreateStatus", "CreateStatus");
+const CreateEquipment = lazyLoad(
+  "./routes/Create/CreateEquipment",
+  "CreateEquipment"
+);
+const CreateUser = lazyLoad("./routes/Create/CreateUser", "CreateUser");
+const CreateEquipmentType = lazyLoad(
+  "./routes/Create/CreateEquipmentType",
+  "CreateEquipmentType"
+);
+const CreateTicketHistory = lazyLoad(
+  "./routes/Create/CreateTicketHistory",
+  "CreateTicketHistory"
+);
+const DetailTicket = lazyLoad("./routes/Detail/DetailTicket", "DetailTicket");
+const DetailEquipment = lazyLoad(
+  "./routes/Detail/DetailEquipment",
+  "DetailEquipment"
+);
 import { ListEquipments } from "./routes/List/ListEquipments";
 import { ListEquipmentsType } from "./routes/List/ListEquipmentsType";
 import { DetailUser } from "./routes/Detail/DetailUser";
@@ -30,66 +44,177 @@ import { UpdateEquipmentType } from "./routes/Update/UpdateEquipmentType";
 import { UpdateUser } from "./routes/Update/UpdateUser";
 import { UpdatePassword } from "./routes/Update/UpdatePassword";
 import { ListTickets } from "./routes/List/ListTickets";
+import { ProtectedRoute } from "./components/ProtectedRoute/ProtectedRoute";
+import { Suspense, lazy } from "react";
 
 function App() {
   return (
     <AuthenticationProvider>
       <div className="flex flex-col h-screen overflow-hidden">
         <ToastContainer />
-        <BrowserRouter>
-          <Routes>
-            <Route path="" element={<Header />}>
-              <Route path="" element={<Login />} />
-              <Route path="/home" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              {/* Ticket */}
-              <Route path="/ticket" element={<ListTickets />} />
-              <Route path="/ticket/new" element={<CreateTicket />} />
-              <Route path="/ticket/:id" element={<DetailTicket />} />
-              <Route
-                path="/ticket/ticket_history/:id"
-                element={<CreateTicketHistory />}
-              />
-              {/* Sector */}
-              <Route path="/sector/new" element={<CreateSector />} />
-              <Route path="/sector" element={<ListSectors />} />
-              <Route path="/sector/update/:id" element={<UpdateSector />} />
-              {/* Status */}
-              <Route path="/status/new" element={<CreateStatus />} />
-              <Route path="/status/:id" element={<DetailStatus />} />
-              <Route path="/status/update/:id" element={<UpdateStatus />} />
-              <Route path="/status" element={<ListStatuses />} />
-              {/* Equipment */}
-              <Route path="/equipment/new" element={<CreateEquipment />} />
-              <Route path="/equipment/:id" element={<DetailEquipment />} />
-              <Route
-                path="/equipment/update/:id"
-                element={<UpdateEquipment />}
-              />
-              <Route path="/equipment" element={<ListEquipments />} />
-              {/* User */}
-              <Route path="/user" element={<ListUsers />} />
-              <Route path="/user/detail/:id" element={<DetailUser />} />
-              <Route path="/user/update/:id" element={<UpdateUser />} />
-              <Route path="/user/new" element={<CreateUser />} />
-              <Route path="/user/password/:id" element={<UpdatePassword />} />
-              {/* Equipment Type */}
-              <Route path="/equipment_type" element={<ListEquipmentsType />} />
-              <Route
-                path="/equipment_type/:id"
-                element={<DetailEquipmentType />}
-              />
-              <Route
-                path="/equipment_type/new"
-                element={<CreateEquipmentType />}
-              />
-              <Route
-                path="/equipment_type/update/:id"
-                element={<UpdateEquipmentType />}
-              />
-            </Route>
-          </Routes>
-        </BrowserRouter>
+        <Suspense>
+          <BrowserRouter>
+            <Routes>
+              <Route path="" element={<Header />}>
+                <Route path="" element={<Login />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                {/* Ticket */}
+                <Route path="/ticket" element={<ListTickets />} />
+                <Route path="/ticket/new" element={<CreateTicket />} />
+                <Route path="/ticket/:id" element={<DetailTicket />} />
+                <Route
+                  path="/ticket/ticket_history/:id"
+                  element={<CreateTicketHistory />}
+                />
+                {/* Sector */}
+                <Route
+                  path="/sector/new"
+                  element={
+                    <ProtectedRoute>
+                      <CreateSector />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/sector"
+                  element={
+                    <ProtectedRoute>
+                      <ListSectors />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/sector/update/:id"
+                  element={
+                    <ProtectedRoute>
+                      <UpdateSector />
+                    </ProtectedRoute>
+                  }
+                />
+                {/* Status */}
+                <Route
+                  path="/status/new"
+                  element={
+                    <ProtectedRoute>
+                      <CreateStatus />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/status/:id"
+                  element={
+                    <ProtectedRoute>
+                      <DetailStatus />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/status/update/:id"
+                  element={
+                    <ProtectedRoute>
+                      <UpdateStatus />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/status"
+                  element={
+                    <ProtectedRoute>
+                      <ListStatuses />
+                    </ProtectedRoute>
+                  }
+                />
+                {/* Equipment */}
+                <Route
+                  path="/equipment/new"
+                  element={
+                    <ProtectedRoute>
+                      <CreateEquipment />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/equipment/:id"
+                  element={
+                    <ProtectedRoute>
+                      <DetailEquipment />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/equipment/update/:id"
+                  element={
+                    <ProtectedRoute>
+                      <UpdateEquipment />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/equipment"
+                  element={
+                    <ProtectedRoute>
+                      <ListEquipments />
+                    </ProtectedRoute>
+                  }
+                />
+                {/* User */}
+                <Route
+                  path="/user"
+                  element={
+                    <ProtectedRoute>
+                      <ListUsers />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route path="/user/detail/:id" element={<DetailUser />} />
+                <Route path="/user/update/:id" element={<UpdateUser />} />
+                <Route
+                  path="/user/new"
+                  element={
+                    <ProtectedRoute>
+                      <CreateUser />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route path="/user/password/:id" element={<UpdatePassword />} />
+                {/* Equipment Type */}
+                <Route
+                  path="/equipment_type"
+                  element={
+                    <ProtectedRoute>
+                      <ListEquipmentsType />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/equipment_type/:id"
+                  element={
+                    <ProtectedRoute>
+                      <DetailEquipmentType />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/equipment_type/new"
+                  element={
+                    <ProtectedRoute>
+                      <CreateEquipmentType />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/equipment_type/update/:id"
+                  element={
+                    <ProtectedRoute>
+                      <UpdateEquipmentType />
+                    </ProtectedRoute>
+                  }
+                />
+              </Route>
+            </Routes>
+          </BrowserRouter>
+        </Suspense>
       </div>
     </AuthenticationProvider>
   );
